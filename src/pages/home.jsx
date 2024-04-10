1adc4b3f4288ddf05d243009ebba0861fd5659e7
MADHURYAHAIT <madhurya_202100516@smit.smu.edu.in>
2024-04-10 14:57:07 +0530

    button

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -10,10 +10,10 @@ const Home = () => {
 
        <img width="28px" style={{marginRight:'10px',borderRadius:'3px'}}  src='/images/atrium.jpg'/>
 
-       Department of Ai&Ds
+         Department of Ai&Ds
       </h3>
       <div class="content-text ">Welcome to the site of Artificial Intelligence & Data Science in Sikkim Manipal Institute of Technology, where Passion meets Technology and Algorithms shape Possibilities.</div>
-      <button class="content-button">See Upcoming Events</button>
+      <a href="https://smu.edu.in/smit/dept-faculty/dept-list/dept-of-Artificial-intelligence-and-Data-science.html"><button class="content-button">Visit us to know more</button></a>
      </div>
      <img class="content-wrapper-img" src="https://assets.codepen.io/3364143/glass.png" alt=""/>
     </div>
