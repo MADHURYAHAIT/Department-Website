f237fdc940a54c51644cd3400f0ba5a56cd072c3
MADHURYAHAIT <madhurya_202100516@smit.smu.edu.in>
2024-04-15 22:25:44 +0530

    junior council menbers

--- src/pages/Council.jsx
+++ src/pages/Council.jsx
@@ -30,6 +30,85 @@ const Council = () => {
         
     </div>
 
+    <div class="content-section">
+    <div class="content-section-title">Council Members</div>
+    <section id="team" class="team">
+      <div class="container">
+        <div class="row">
+
+        <div class="col-lg-4 col-md-6">
+            <div class="member" data-aos="fade-up">
+              <div class="pic"><img src="./images/ankit.jpeg" class="img-fluid" alt=""/></div>
+              <div class="member-info">
+                <h4>Ankit Ritu</h4>
+                <span>President</span>
+                <div class="social">
+                  <a href=""><i class="bi bi-twitter"></i></a>
+                  <a href=""><i class="bi bi-facebook"></i></a>
+                  <a href=""><i class="bi bi-instagram"></i></a>
+                  <a href=""><i class="bi bi-linkedin"></i></a>
+                </div>
+              </div>
+            </div>
+          </div>
+
+
+        <div class="col-lg-4 col-md-6">
+            <div class="member" data-aos="fade-up">
+              <div class="pic"><img src="./images/anushree.jpeg" class="img-fluid" alt=""/></div>
+              <div class="member-info">
+                <h4>Anushree Tyagi</h4>
+                <span>Cultural Secretary</span>
+                <div class="social">
+                  <a href=""><i class="bi bi-twitter"></i></a>
+                  <a href=""><i class="bi bi-facebook"></i></a>
+                  <a href=""><i class="bi bi-instagram"></i></a>
+                  <a href=""><i class="bi bi-linkedin"></i></a>
+                </div>
+              </div>
+            </div>
+          </div>
+
+
+          <div class="col-lg-4 col-md-6">
+            <div class="member" data-aos="fade-up">
+              <div class="pic"><img src="./images/rafiullah.jpeg" class="img-fluid" alt=""/></div>
+              <div class="member-info">
+                <h4>Rafiullah Ansari</h4>
+                <span>Treasurer</span>
+                <div class="social">
+                  <a href=""><i class="bi bi-twitter"></i></a>
+                  <a href=""><i class="bi bi-facebook"></i></a>
+                  <a href=""><i class="bi bi-instagram"></i></a>
+                  <a href=""><i class="bi bi-linkedin"></i></a>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6">
+            <div class="member" data-aos="fade-up">
+              <div class="pic"><img src="./images/sambhav.jpeg" class="img-fluid" alt=""/></div>
+              <div class="member-info">
+                <h4>Sambhav Jaiswal</h4>
+                <span>Sports Secretary</span>
+                <div class="social">
+                  <a href=""><i class="bi bi-twitter"></i></a>
+                  <a href=""><i class="bi bi-facebook"></i></a>
+                  <a href=""><i class="bi bi-instagram"></i></a>
+                  <a href=""><i class="bi bi-linkedin"></i></a>
+                </div>
+              </div>
+            </div>
+          </div>
+
+
+
+        </div>
+      </div>
+    </section>
+
+    </div>
     {/* Old Council */}
 
     <div class="content-section">
