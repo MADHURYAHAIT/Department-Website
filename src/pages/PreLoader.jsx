d99eec4240bb83d565565418b63c29656a36bac9
MADHURYAHAIT <madhurya_202100516@smit.smu.edu.in>
2024-04-18 17:06:50 +0530

    fadeout

--- src/pages/PreLoader.jsx
+++ src/pages/PreLoader.jsx
@@ -1,8 +1,18 @@
+import { useState,useEffect } from "react";
 import React from 'react'
 
 const PreLoader = () => {
+  const [fadeOut, setFadeOut] = useState(false);
+  useEffect(() => {
+    // Simulate loading time
+    const timeout = setTimeout(() => {
+      setFadeOut(true); // Set fadeOut to true after some time
+    }, 3000); // Adjust the timeout as needed
+
+    return () => clearTimeout(timeout); // Cleanup function
+  }, []);
   return (
-    <div className='Preloader'>
+    <div className={`Preloader ${fadeOut ? 'fadeOut' : ''}`}>
 
         <img src='./images/Launch.jpg'/>
         <p>
